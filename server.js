9fa0f96f5536fa4cc44cb7dad845eaa22ee99cfb
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-21 15:18:06 +0100

    do task

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var fs = require('fs');
+var stringifFile;
 
-app.get('/', function (req,res) {
-	console.log('Otrzymałem żadanie GET do strony głównej')
-	res.send('Hello GET!');
+app.use(bodyParser.json());
+app.get('/getNote', function (req,res) {
+	fs.readFile('./test.json', 'utf-8', function (err, data) {
+		if (err) throw err;
+		stringifFile = data;
+		res.send(data);
+	});
+});
+
+app.post('/updateNote/:note', function (req,res) {
+	fs.appendFile('./test.json', req.params.note, function (err) {
+		if (err) throw err;
+		console.log('file update');
+		res.send();
+	});
 });
 
 app.use(function (req, res, next) {
