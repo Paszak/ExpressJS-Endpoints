036bef8144abc2db66cd6793e0c0ec7fff184ceb
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-21 14:26:14 +0100

    error handling

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/:id', function (req,res) {
+app.get('/', function (req,res) {
 	console.log('Otrzymałem żadanie GET do strony głównej')
-	res.send('Identyfikator , który zotał dopisany to ' + req.params.id);
+	res.send('Hello GET!');
+});
+
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego , czego żądasz!')
 });
 
 app.listen(3000);
