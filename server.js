8f52e82efe3e7a53097804fe7155d60774cfe2a5
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-21 14:21:11 +0100

    dynamic endpoints

--- server.js
+++ server.js
@@ -1,31 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req,res) {
+app.get('/:id', function (req,res) {
 	console.log('Otrzymałem żadanie GET do strony głównej')
-	res.send('Hello GET!');
+	res.send('Identyfikator , który zotał dopisany to ' + req.params.id);
 });
 
-app.post('/', function (req,res) {
-	console.log('Otrzymałem żadanie POST do strony głównej')
-	res.send('Hello POST!');
-});
-
-app.delete('/del_user', function (req,res) {
-	console.log('Otrzymałem żadanie DELETE do strony głównej')
-	res.send('Hello DELETE!');
-});
-
-app.get('/list_user', function (req,res) {
-	console.log('Otrzymałem żadanie GET do strony /list_user')
-	res.send('Strona z listą użytkowników');
-});
-
-app.get('/ab*cd', function (req,res) {
-	console.log('Otrzymałem żadanie GET do strony ab*cd')
-	res.send('Wzór pasuje');
-});
-
-var server = app.listen(3000, function () {
-	console.log('przykładowa aplikacja')
-});
+app.listen(3000);
