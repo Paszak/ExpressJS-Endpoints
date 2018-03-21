5efb40f955f82836bcd6627345714b85f9f65b6a
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-21 14:17:06 +0100

    test endpoints

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 var express = require('express');
-
 var app = express();
 
 app.get('/', function (req,res) {
-	res.send('Hello world');
+	console.log('Otrzymałem żadanie GET do strony głównej')
+	res.send('Hello GET!');
+});
+
+app.post('/', function (req,res) {
+	console.log('Otrzymałem żadanie POST do strony głównej')
+	res.send('Hello POST!');
+});
+
+app.delete('/del_user', function (req,res) {
+	console.log('Otrzymałem żadanie DELETE do strony głównej')
+	res.send('Hello DELETE!');
+});
+
+app.get('/list_user', function (req,res) {
+	console.log('Otrzymałem żadanie GET do strony /list_user')
+	res.send('Strona z listą użytkowników');
+});
+
+app.get('/ab*cd', function (req,res) {
+	console.log('Otrzymałem żadanie GET do strony ab*cd')
+	res.send('Wzór pasuje');
 });
 
 var server = app.listen(3000, function () {
-	console.log("przykładowa aplikacja")
+	console.log('przykładowa aplikacja')
 });
